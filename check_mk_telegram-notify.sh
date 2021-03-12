f0f5598fd27adc31112597bf3678f90e9ad89023
Thorian93 <info@thorian93.de>
2021-03-12 20:36:53 +0100

    Add ability to read chatid from Checkmk custom attribute.

--- check_mk_telegram-notify.sh
+++ check_mk_telegram-notify.sh
@@ -9,14 +9,21 @@
 
 # Telegram API Token
 # Find telegram bot named "@botfarther", type /mybots, select your bot and select "API Token" to see your current token
-TOKEN='TELEGRAM_API_TOKEN_WITHOUT_BOT_PREFIX'
+TOKEN='CHANGEME'
+
 # Telegram Chat-ID or Group-ID
 # Open "https://api.telegram.org/bot<YOUR_TOKEN>/getUpdates" inside your Browser and send a HELLO to your bot, refresh side
-CHAT_ID='TELEGRAM_GROUP_OR_CHAT-ID'
+CHAT_ID='CHANGEME'
 
 # Write Check_MK output to a temporary file, delete depricated macros and create variable OUTPUT
 env | grep NOTIFY_ | grep -v "This macro is deprecated" | sort > $OMD_ROOT/tmp/telegram.out
 OUTPUT=$OMD_ROOT/tmp/telegram.out
+
+# Try and use the custom Attribute from checkmk in case is not explicitly set above
+if [ ${CHAT_ID} == "CHANGEME" ]
+then
+  CHAT_ID=$(grep NOTIFY_CONTACT_TELEGRAM_CHAT_ID $OUTPUT | cut -d'=' -f2)
+fi
 
 # Read OUTPUT variable and create some more variables for every text-part you want to use afterwards
 HOSTNAME=$(grep NOTIFY_HOSTNAME $OUTPUT | cut -d'=' -f2)
